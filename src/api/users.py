26f76a50cb66361c0ceba779617a74577fd9ad25
Mcray3000 <69219980+Mcray3000@users.noreply.github.com>
2024-11-18 20:48:27 -0800

    Class User updated to have proper fields with constraints and defaults, added proper http code status, and http code and contrains for 400 errors are handled by pydantic now

--- src/api/users.py
+++ src/api/users.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import sqlalchemy
 import logging
 from src import database as db
@@ -14,33 +15,34 @@ router = APIRouter(
 )
 
 class User(BaseModel):
-    name: str
-    location: str | None = None
+    name: str = Field(..., min_length=1)
+    location: Optional[str] = Field(default="Calpoly SLO")  # Default "Calpoly SLO"
+    longitude: Optional[float] = Field(default=120.6625)  # Default longitude
+    latitude: Optional[float] = Field(default=35.3050)    # Default latitude
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(new_user: User):
     """
     Creates a new user in the system.
     Returns the user_id of the created user.
     """
-    user_info = {"name": new_user.name, "location": new_user.location}
-    with db.engine.begin() as conn:
-        try:
+    user_info = {"name": new_user.name, "location": new_user.location,
+                 "long": new_user.longitude, "lat": new_user.latitude}
+    try:
+        with db.engine.begin() as conn:
             user_id = conn.execute(sqlalchemy.text("""
-                    INSERT INTO users (name, location)
-                    VALUES (:name, :location)
+                    INSERT INTO users (name, location, longitude, latitude)
+                    VALUES (:name, :location, :long, :lat)
                     RETURNING user_id
                     """
                 ), user_info).scalar_one()
             
             return {"user_id": user_id}
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                            detail="Failed to create user.")
             
-        except Exception as e:
-            logger.exception(f"Error creating user: {e}")
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="Failed to create user"
-            )
     
 @router.post("/{user_id}/preferences", status_code=status.HTTP_201_CREATED)
 def add_preferences(user_id: int, budget: int):
